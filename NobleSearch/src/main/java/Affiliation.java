6ec75e52f69e3135d9b882a49e88502e9dc3d48c
Pranav Kalidindi <pvk16@ic.ac.uk>
2017-11-17 22:47:09 +0000

    affiliation gson

---
+++ NobleSearch/src/main/java/Affiliation.java
@@ -0,0 +1,15 @@
+public class Affiliation {
+
+  String name;
+  String city;
+
+  public Affiliation(String name, String city, String country) {
+    this.name = name;
+    this.city = city;
+    this.country = country;
+  }
+
+  String country;
+
+
+}
