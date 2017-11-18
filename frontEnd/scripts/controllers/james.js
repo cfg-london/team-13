95539a953c5f04b37c41e585647db62c33153032
dragosMartac <dom17@ray01.doc.ic.ac.uk>
2017-11-18 07:00:46 +0000

    Attempt to load links

--- frontEnd/scripts/controllers/james.js
+++ frontEnd/scripts/controllers/james.js
@@ -1,51 +1,53 @@
 myApp.controller('James', function ($scope, $rootScope, $location,$window,$window,$sce) {
 
   $scope.init = function () {
-    console.log("");
-    $scope.apiURL ="";
+    console.log("initController");
+    $scope.apiURL ="http://localhost:3000/similar?url=/nobel_prizes/medicine/laureates/1962/";
     getdata($scope.apiURL);
   };
 
   console.log("proba");
   $scope.data = [
-            { title:"Titlu1",
-              description:"Description1",
-              imageURL:"https://www.nobelprize.org/images/recommend/medicine/medicine-short-facts.jpg",
+            { //title:"Titlu1",
+              //description:"Description1",
+              //imageURL:"https://www.nobelprize.org/images/recommend/medicine/medicine-short-facts.jpg",
               link:""
               },
-            { 
-              title:"Titlu2",
-              description:"Description2",
-              imageURL:"pic/pic2",
+            {
+              //title:"Titlu2",
+              //description:"Description2",
+              //imageURL:"pic/pic2",
               link:"/"
               },
-            { 
-              title:"Titlu3",
-              description:"Description3",
-              imageURL:"pic/pic3",
+            {
+              //title:"Titlu3",
+              //description:"Description3",
+              //imageURL:"pic/pic3",
               link:"/"
             }];
 
 
-  generateRecomandations ($scope.data.length,); 
+
   function generateRecomandations (nr){
-     console.log("is this even called..");
+     console.log("is this even called.." + nr);
      var recommendationsTags = "";
+     $route.reload()
      for(var i=0; i < nr; i++){
-       recommendationsTags +='<div class="recommended-slider-item slick-slide slick-active" data-slick-index="1" aria-hidden="false" tabindex="0" role="option" aria-describedby="slick-slide01" style="width: 144px;"><a href="' 
-                           + $scope.data[i].link 
-                           + '" tabindex="0"><img class="recommended-slider-img" src="' 
-                           + $scope.data[i].imageURL 
-                           + '"><a class="recommended-slider-title" href="' 
-                           + $scope.data[i].link 
-                           +' tabindex="0">'
-                           + $scope.data[i].title 
-                           + '</a>'
-                           + '<p class="recommended-slider-descr">'
-                           + $scope.data[i].description
-                           +'</p></div>';
+       console.log($scope.data[i])
+       recommendationsTags +='<div class="recommended-slider-item slick-slide slick-active" data-slick-index="1" aria-hidden="false" tabindex="0" role="option" aria-describedby="slick-slide01" style="width: 144px;"><a href="'
+                           + $scope.data[i].link
+                           + '" tabindex="0">' //<img class="recommended-slider-img" src="'
+                           //+ $scope.data[i].imageURL
+                           + '"<a class="recommended-slider-title" href="'
+                           + $scope.data[i].link
+                           +' tabindex="0">' + "Title</a>";
+                           //+ $scope.data[i].title
+                           //+ '</a>'
+                           //+ '<p class="recommended-slider-descr">'
+                          // + $scope.data[i].description
+                          // +'</p></div>';
        }
-       
+
        console.log(recommendationsTags);
        $scope.recommendations = $sce.trustAsHtml(recommendationsTags);
      }
@@ -60,9 +62,11 @@ myApp.controller('James', function ($scope, $rootScope, $location,$window,$windo
     function getdata (apiURL){
         console.log("getdata");
         $.get(apiURL, function(data, status){
-                $scope.data = JSON.parse(data);
+                //console.log(data)
+                $scope.data = data
                 alert("Data: " + data + "\nStatus: " + status);
                 $scope.$apply();
+                generateRecomandations ($scope.data.length);
             });
     }
 
