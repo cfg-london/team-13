6816a5140ee9d6d4bb1f8f4a4dc2799010bd5598
dragosMartac <dom17@ray01.doc.ic.ac.uk>
2017-11-18 04:12:01 +0000

    Add query to retrieve similar urls

--- index.js
+++ index.js
@@ -5,16 +5,36 @@ const app = express()
 var con = mysql.createConnection({
   host: '54.77.29.175',
   user: 'root',
-  password: 'toor'
+  password: 'toor',
+  database: 'recommendations'
 })
 
-app.get('/', async function(req, res) {
-  let hasFailed = await con.connect()
-  if (hasFailed) {
-    res.send('Oh no')
-  } else {
-    res.send('Connected')
-  }
+con.connect(function(err) {
+  app.get('/', async function(req, res) {
+    if (err) {
+      res.send('Oh no')
+    } else {
+      res.send('Connected')
+    }
+  })
+
+  app.get('/similar', async function(req, res) {
+    if (err) {
+      res.send('Oh no')
+    } else {
+      con.query("SELECT u2.url FROM urls \
+                  u1 JOIN ranks r ON r.from=u1.id \
+                     JOIN urls u2 ON r.to=u2.id \
+                  WHERE u1.url='"+ req.query.url +"'", function(err, result, field) {
+
+        res.send(result);
+        console.log(err + "|"+ field);
+      })
+    }
+  })
 })
+
+
+
 
 app.listen(3000)
